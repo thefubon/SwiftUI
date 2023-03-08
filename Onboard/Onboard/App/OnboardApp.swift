cf486487e26960d31ea93e6db4a623874b0f3edb
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2023-03-08 12:33:46 +0300

    Initial

---
+++ Onboard/Onboard/App/OnboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardApp.swift
+//  Onboard
+//
+//  Created by iMac on 08.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct OnboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
